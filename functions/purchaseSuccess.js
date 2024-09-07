35c144ef816c63d1e6700331e525b64accef10c2
Ana MacBook <anac2828@yahoo.com>
2024-09-07 10:22:17 -0700

    TODO: finish sendGrind set up

--- functions/purchaseSuccess.js
+++ functions/purchaseSuccess.js
@@ -1,4 +1,4 @@
-import Stripe from "stripe";
+import Stripe from 'stripe';
 const stripeKey = process.env.VITE_REACT_APP_STRIPE_SECRET_KEY;
 const webhookKey = process.env.VITE_WEBHOOK_SECRET_KEY;
 const stripe = new Stripe(stripeKey);
@@ -9,18 +9,23 @@ export const handler = async function (event, context) {
     //   Check that event comes from stripe
     const stripeEvent = stripe.webhooks.constructEvent(
       body,
-      headers["stripe-signature"],
+      headers['stripe-signature'],
       webhookKey
     );
 
-    if (stripeEvent.type === "checkout.session.completed") {
+    if (stripeEvent.type === 'checkout.session.completed') {
       const eventObject = stripeEvent.data.object;
       //  all line items that belong to the checkout session
       const items = await stripe.checkout.sessions.listLineItems(
         eventObject.id,
-        { expand: ["data.price.product"] }
+        { expand: ['data.price.product'] }
       );
-      console.log(items.data[0].price);
+
+      // Data for fulfillment
+      const { product: productData } = items.data[0].price;
+      const { name: productName } = product;
+
+      // TODO Finish sendgrid setup
     }
 
     return {
